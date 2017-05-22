2cdede2982a91321bbd465992e16a79e52148019
Itay Bookstein <ibookstein@gmail.com>
2017-05-22 19:36:38 +0300

    templated implementation on bit count and adapted code to work generically for different bit counts, still needs tidying up & testing

--- TSHash/Main.cpp
+++ TSHash/Main.cpp
@@ -6,8 +6,8 @@
 
 int main()
 {
-	tshash::PARAMETERS parameters{ {1, 1}, {{1, 1}, {1, 1}} };
-	tshash::Hash hash(parameters);
+	tshash::PARAMETERS<72> parameters{ {1}, {{1}} };
+	tshash::Hash<72> hash(parameters);
 	
 	std::array<uint8_t, 10> arr{ 1, 2, 3 };	
 	hash.update(arr.data(), arr.size());
--- TSHash/TSHash.hpp
+++ TSHash/TSHash.hpp
@@ -1,42 +1,141 @@
 #pragma once
 
+#include <array>
 #include <cstdint>
 #include <iostream>
+#include <iomanip>
 
 
 namespace tshash {
 
-struct VECTOR
+template<size_t Bits>
+struct BIT_VECTOR
 {
-	uint64_t low_part;
-	uint64_t high_part;
+	std::array<uint64_t, (Bits + 63) / 64> data;
 };
 
-VECTOR& operator >>= (VECTOR& v, uint32_t shift);
-VECTOR operator >> (const VECTOR& v, uint32_t shift);
-VECTOR& operator ^= (VECTOR& lhs, const VECTOR& rhs);
-VECTOR operator ^ (const VECTOR& lhs, const VECTOR& rhs);
-std::ostream& operator << (std::ostream& os, const VECTOR& v);
+template<size_t Bits>
+uint32_t bit_scan_forward(const BIT_VECTOR<Bits>& v)
+{
+	for (size_t i = 0; i < v.data.size(); ++i)
+	{
+		unsigned long set_bit_index;
+		if (_BitScanForward64(&set_bit_index, v.data[i]))
+		{
+			return static_cast<uint32_t>(set_bit_index + 64 * i);
+		}
+	}
+	return -1;
+}
 
+template<size_t Bits>
+BIT_VECTOR<Bits>& operator >>= (BIT_VECTOR<Bits>& v, uint32_t shift)
+{
+	for (; shift >= 64; shift -= 64)
+	{
+		for (size_t i = 0; i < v.data.size() - 1; ++i)
+		{
+			v.data[i] = v.data[i + 1];
+		}
+		v.data.back() = 0;
+	}
+
+	for (size_t i = 0; i < v.data.size() - 1; ++i)
+	{
+		auto& current_word = v.data[i];
+		current_word >>= shift;
+		current_word |= (v.data[i + 1] << (64 - shift));
+	}
+	v.data.back() >>= shift;
+
+	return v;
+}
+
+template<size_t Bits>
+BIT_VECTOR<Bits> operator >> (const BIT_VECTOR<Bits>& v, uint32_t shift)
+{
+	auto result = v;
+	result >>= shift;
+	return result;
+}
+
+template<size_t Bits>
+BIT_VECTOR<Bits>& operator ^= (BIT_VECTOR<Bits>& lhs, const BIT_VECTOR<Bits>& rhs)
+{
+	for (size_t i = 0; i < lhs.data.size(); ++i)
+	{
+		lhs.data[i] ^= rhs.data[i];
+	}
+	return lhs;
+}
+
+template<size_t Bits>
+BIT_VECTOR<Bits> operator ^ (const BIT_VECTOR<Bits>& lhs, const BIT_VECTOR<Bits>& rhs)
+{
+	auto result = lhs;
+	result ^= rhs;
+	return result;
+}
+
+template<size_t Bits>
+std::ostream& operator << (std::ostream& os, const BIT_VECTOR<Bits>& v)
+{
+	for (size_t i = v.data.size() - 1; i != 0; --i)
+	{
+		os << std::hex << std::setfill('0') << std::setw(2 * sizeof(v.data[i])) << v.data[i];
+	}
+	os << std::hex << std::setfill('0') << std::setw(2 * (Bits % 64) / 8) << v.data.front();
+	return os;
+}
+
+template<size_t Bits>
 struct PARAMETERS
 {
-	VECTOR initial_state;
-	VECTOR polynomials[2];
+	BIT_VECTOR<Bits> initial_state;
+	BIT_VECTOR<Bits> polynomials[2];
 };
 
+template<size_t Bits>
 class Hash
 {
 public:
-	constexpr explicit Hash(const PARAMETERS& parameters);
-	Hash(const PARAMETERS& parameters, uint8_t* data, size_t size);
-	void update(uint8_t* data, size_t size);
-	VECTOR digest() { return m_state; }
+	constexpr explicit Hash(const PARAMETERS<Bits>& parameters) : 
+		m_parameters(parameters),
+		m_state(parameters.initial_state)
+	{}
+
+	Hash(const PARAMETERS<Bits>& parameters, uint8_t* data, size_t size) : 
+		Hash(parameters)
+	{
+		update(data, size);
+	}
+
+	void update(uint8_t* data, size_t size)
+	{
+		for (size_t i = 0; i < size; ++i)
+		{
+			uint8_t current_byte = data[i];
+			for (size_t j = 0; j < 8; ++j, current_byte >>= 1)
+			{
+				// TODO: Consider loading m_state to a local
+				_update_bit(current_byte & 1);
+			}
+		}
+	}
+
+	BIT_VECTOR<Bits> digest() { return m_state; }
 
 private:
-	void _update_bit(size_t bit);
+	void _update_bit(size_t bit)
+	{
+		const uint32_t shift_amount = bit_scan_forward(m_state) + 1;
 
-	PARAMETERS m_parameters;
-	VECTOR m_state;
+		m_state >>= shift_amount;
+		m_state ^= m_parameters.polynomials[bit];
+	}
+
+	PARAMETERS<Bits> m_parameters;
+	BIT_VECTOR<Bits> m_state;
 };
 
 }
